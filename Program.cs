eac34c137a34d4545d118b80e35e7529895891c2
Gizem <gizemustahuseyin123@gmail.com>
2023-01-24 21:33:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._17._1_Cevrimler2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayi giriniz:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 2;
+            do 
+            //herhangi bir koşula bakmaksızın olduğu gibi çalıştırır.
+            //,koşul ifadesi başlamgıç ifadeszi geçerli değil.
+            {
+                Console.Write("{0,3}",i);
+                i += 2;
+            } 
+            while (i <= n);
+            Console.ReadKey();
+        }
+    }
+}
